ecf0ee4d328d66c5fdc40fedd9f9c6eb83dfb75f
Mehdi Bounya <mehdi.bounya@gmail.com>
2022-01-14 15:35:05 +0100

    Use config for routes instead of annotations

--- tests/Fixture/App/Controller/ExampleController.php
+++ tests/Fixture/App/Controller/ExampleController.php
@@ -3,14 +3,11 @@
 namespace SymfonyBundles\JsonRequestBundle\Tests\Fixture\App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class ExampleController
 {
-    /**
-     * @Route("/")
-     */
+
     public function index(Request $request): JsonResponse
     {
         return new JsonResponse($request->request->all());
